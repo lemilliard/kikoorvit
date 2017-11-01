64e6525c269657dc7b7f0d49df9015dae36c72cb
Thomas Kint <thomaskint.pro@gmail.com>
2017-11-01 16:21:44 +0100

    Moved images in static

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <v-app toolbar>
     <v-toolbar app class="primary" dark>
-      <img src="/static/Niglo%2001.png" style="max-width: 90px; cursor: pointer;" @click.stop="niglo = true"/>
+      <img src="/static/img/Niglo%2001.png" style="max-width: 90px; cursor: pointer;" @click.stop="niglo = true"/>
       <v-toolbar-title>Kikoorvit</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-title>Le Bon Djo</v-toolbar-title>
-      <img src="/static/Fist%2001.png" style="max-width: 60px; cursor: pointer;" @click.stop="fist = true"/>
+      <img src="/static/img/Fist%2001.png" style="max-width: 60px; cursor: pointer;" @click.stop="fist = true"/>
     </v-toolbar>
     <main>
       <v-content>
-        <v-dialog v-model="niglo" width="600px">
+        <v-dialog v-model="niglo" max-width="600px">
           <v-card>
             <v-layout justify-center align-center>
-              <img src="/static/Niglo%2002.gif"/>
+              <img src="/static/img/Niglo%2002.gif"/>
             </v-layout>
             <v-card-title primary-title>
               <h2>La chaussure Kikoorvit</h2>
             </v-card-title>
           </v-card>
         </v-dialog>
-        <v-dialog v-model="fist" width="320px">
+        <v-dialog v-model="fist" max-width="320px">
           <v-card>
             <v-layout justify-center align-center>
-              <img src="/static/Fist%2001.png"/>
+              <img src="/static/img/Fist%2001.png"/>
             </v-layout>
             <v-card-title primary-title>
               <h2>J'en veuuux</h2>
@@ -31,7 +31,7 @@
         </v-dialog>
         <v-container>
           <v-layout justify-center>
-            <img :src="`/static/Kikoorvit ${speedChar}.gif`" :style="`max-width: ${windowSize.x}px`"/>
+            <img :src="`/static/img/Kikoorvit ${speedChar}.gif`" :style="`max-width: ${windowSize.x}px`"/>
           </v-layout>
         </v-container>
       </v-content>
